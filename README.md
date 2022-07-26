87a77c423934f2ce0b919019b0d47b7a26bd21c4
Pavlo Surzhenko <pavel.surzhenko@icloud.com>
2022-07-26 15:09:48 +0300

    second version gulp

--- README.md
+++ README.md
@@ -4,6 +4,22 @@
 >./src/styles/\*\*/\*.less  
 >./src/scripts/\*\*/\*.js
 
+## 
+Минимизация HTML  
+Компиляция препроцессоров LESS, SASS  
+Минимизация CSS  
+Автоматическое добавление префиксов CSS  
+Транспиляция языка Type Script  
+Преобразования кода ECMAScript 2015 + в обратно совместимую версию JavaScript с помощью Babel
+Минимизация JavaScript
+Объединение нескольких файлов JavaScript в один
+Сжатие изображений
+Отслеживание новых изображений, которые еще не были сжаты
+Отслеживание изменений в файлах и автоматический запуск повторной обработки
+Генерация sourcemaps
+Отображение размеров файлов в терминале
+Локальный сервер с автоматическим обновлением страницы при изменении файлов
+
 ## Инструкция:
 1.Скачать все файлы в любою директорию  
 2.Ввести в терминале команду: npm i  
@@ -11,11 +27,20 @@
 
 ## Установленные NPM пакеты:
 [gulp](https://www.npmjs.com/package/gulp) Сборщик Gulp  
-[gulp-less](https://www.npmjs.com/package/gulp-less) Компиляция Less файлов  
+[gulp-htmlmin](https://www.npmjs.com/package/gulp-htmlmin) Минификация HTML файлов  
+[gulp-sass](https://www.npmjs.com/package/gulp-sass) Компиляция Sass и Scss файлов    
+[gulp-less](https://www.npmjs.com/package/gulp-less) Компиляция Less файлов 
+[gulp-autoprefixer](https://www.npmjs.com/package/gulp-autoprefixer) Сборщик Gulp   
+[gulp-uglify](https://www.npmjs.com/package/gulp-uglify) Сжатие и оптимизация JS  
+[gulp-typescript](https://www.npmjs.com/package/gulp-typescript) Преобразует Type Script в Java Script   
 [gulp-babel](https://www.npmjs.com/package/gulp-babel) Компиляция JS для старых браузеров  
 [gulp-concat](https://www.npmjs.com/package/gulp-concat) объединение файлов в один  
-[gulp-uglify](https://www.npmjs.com/package/gulp-uglify) Сжатие и оптимизация JS  
 [gulp-rename](https://www.npmjs.com/package/gulp-rename) Изменение имени файлов  
 [gulp-clean-css](https://www.npmjs.com/package/gulp-clean-css) Минимизация css файлов  
 [del](https://www.npmjs.com/package/del) Удаления каталогов и файлов  
+[gulp-sourcemaps](https://www.npmjs.com/package/gulp-sourcemaps) Карта строк кода для инструментов разработчика  
+[gulp-imagemin](https://www.npmjs.com/package/gulp-imagemin) Сжатие изображений 
+[gulp-newer](https://www.npmjs.com/package/gulp-newer) Отслеживание только новых файлов 
+[browser-sync](https://www.npmjs.com/package/browser-sync) Автоматическое обновление сайта при изменении файлов  
+
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,133 @@
 //подключение модулей
 import gulp from 'gulp';
-import less from 'gulp-less';
-import {deleteAsync} from 'del';
+//import less from 'gulp-less';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
 import rename from 'gulp-rename';
 import GulpCleanCss from 'gulp-clean-css';
+import ts from 'gulp-typescript'
 import babel from 'gulp-babel';
 import uglify from 'gulp-uglify';
 import concat from 'gulp-concat';
+import sourcemaps from 'gulp-sourcemaps';
+import autoprefixer from 'gulp-autoprefixer';
+import imagemin from 'gulp-imagemin';
+import htmlmin from 'gulp-htmlmin';
+import newer from 'gulp-newer';
+import browserSync from 'browser-sync';
+import {deleteAsync} from 'del';
+
 
 
 //пути к файлам
 const paths = {
+    html: {  
+        src: 'src/*.html', 
+        dest: 'dist'  
+    },
     styles: {  //стили
-        src: 'src/styles/**/*.less', //путь для рабочего файла с папки срс
+        src: ['src/styles/**/*.less', 'src/styles/**/*.sass', 'src/styles/**/*.scss'], //путь для рабочего файла с папки срс
         dest: 'dist/css/'  //путь куда сохраняем результат
     },
     scripts: {  //скрипты
-        src: 'src/scripts/**/*.js',  //путь для рабочего файла с папки срс
+        src: ['src/scripts/**/*.js', 'src/scripts/**/*.ts'],  //путь для рабочего файла с папки срс
         dest: 'dist/js/'  //путь куда сохраняем результат
     },
+    images: {
+        src: 'src/img/**',
+        dest: 'dist/img'
+    }
 }
 
 //очистка папки 
 function clean() {
-    return deleteAsync(['dist'])
+    return deleteAsync(['dist/*', '!dist/img'])
+}
+
+//Минимизация Html
+function html() {
+    return gulp.src(paths.html.src)
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest(paths.html.dest))
+    .pipe(browserSync.stream())
 }
 
 //стили
+const sass = gulpSass(dartSass);
 function styles() {
     return gulp.src(paths.styles.src)
-        .pipe(less())
-        .pipe(GulpCleanCss())
+        .pipe(sourcemaps.init())
+        //.pipe(less())  
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+			cascade: false
+		}))
+        .pipe(GulpCleanCss({
+            level: 2
+        }))
         .pipe(rename({
             basename: 'main',
             suffix: '.min'
         }))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(paths.styles.dest))
+        .pipe(browserSync.stream())
 }
 
 //скрипты
 function scripts() {
-    return gulp.src(paths.scripts.src, {
-        sourcemaps: true
-    })
-    .pipe(babel())
+    return gulp.src(paths.scripts.src)
+    .pipe(sourcemaps.init())
+    .pipe(ts({  //ts
+        noImplicitAny: true,
+        outFile: 'main.min.js'
+    }))
+    .pipe(babel({
+        presets: ['@babel/env']
+    }))
     .pipe(uglify())
     .pipe(concat('main.min.js'))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(paths.scripts.dest))
+    .pipe(browserSync.stream())
 
 }
 
+//Сжатие картинок
+function img() {
+    return gulp.src(paths.images.src)
+    .pipe(newer(paths.images.dest))
+    .pipe(imagemin({
+        progressive: true,
+        optimizationLevel: 5
+    }))
+    .pipe(gulp.dest(paths.images.dest))
+}
+
+//отслеживание изменения в файлах и запуск лайв сервера
 function watch() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist/"
+        }
+    })
+    gulp.watch(paths.html.dest).on('change', browserSync.reload)
+    gulp.watch(paths.html.src, html)
     gulp.watch(paths.styles.src, styles)
     gulp.watch(paths.scripts.src, scripts)
+    gulp.watch(paths.images.src, img)
 }
 
-
-const build = gulp.series(clean, gulp.parallel(styles, scripts), watch)
-
+//для ручного запуска
 export { clean as clean}
+export { img as img}
+export { html as html}
 export { styles as styles}
 export { scripts as scripts}
 export { watch as watch}
 export { build as build}
 export { build as default}
+
+//конечный билд
+const build = gulp.series(clean, html, gulp.parallel(styles, scripts, img), watch)
+
